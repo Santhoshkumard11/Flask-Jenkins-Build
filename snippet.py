03be44eeb610349eb771834f8be485ca209ba74e
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2021-08-14 12:45:45 +0530

    Update snippet.py

--- snippet.py
+++ snippet.py
@@ -1,21 +1,45 @@
-def check_posted_data(postedData, functionName):
-    if (functionName == "add" or functionName == "subtract" or functionName == "multiply"):
-        if "x" not in postedData or "y" not in postedData:
-            return 301 #Missing parameter
+import argparse
+
+def get_args():
+    
+    parser = argparse.ArgumentParser(description='Pass two numbers and a operation to perform on them')
+    
+    parser.add_argument('--first_number', type=int, help='First Number')
+    parser.add_argument('--second_number',type=int, help='Second Number')
+    parser.add_argument('--operation', type=str, help='operation')
+
+    args = parser.parse_args()
+
+    return {"x": args.first_number, "y": args.second_number}, args.operation
+
+
+def check_posted_data(posted_data, function_name):
+    if (function_name == "add" or function_name == "subtract" or function_name == "multiply"):
+        if "x" not in posted_data or "y" not in posted_data:
+            return 301  # Missing parameter
         else:
             return 200
-    elif (functionName == "division"):
-        if "x" not in postedData or "y" not in postedData:
+    elif (function_name == "division"):
+        if "x" not in posted_data or "y" not in posted_data:
             return 301
-        elif int(postedData["y"])==0:
+        elif int(posted_data["y"]) == 0:
             return 302
         else:
             return 200
-          
+
+def main():
+    
+    data, operation = get_args()
+
+    print(data)
+
+    result = check_posted_data(data, operation)
+    
+    print(f"The result of the check is {result}")
+
 
 if __name__ == "__main__":
-  
-  data = {"x": 23, "y": 76}
-  result = check_posted_data(data, "subtract")
-  print(f"The sum of {data.get("x")} and {data.get("y")} is {result}")
-  
+
+    main()
+    
+    
